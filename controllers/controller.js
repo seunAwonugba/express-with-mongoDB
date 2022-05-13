680aef6f7c4d56083bd76e7ca8e413d3187c3232
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-05-13 20:24:19 +0100

    implement update and delete operations

--- controllers/controller.js
+++ controllers/controller.js
@@ -41,28 +41,66 @@ const getAllData = async (req, res) => {
     }
 };
 
-const getDataById = (req, res) => {
-    console.log(req.params);
-    res.status(200).json({
-        success: true,
-        data: "get data from db by id",
-    });
-};
-const updateData = (req, res) => {
-    console.log(req.body);
-    console.log(req.params);
-    res.status(201).json({
-        success: true,
-        data: "update data on the db",
-    });
+//fetch data from database by ID
+//step 1 get the id of an element which is called document here using req.params.id by using the .findById() method
+const getDataById = async (req, res) => {
+    try {
+        const dataToFetchById = await Model.findById(req.params.id);
+        res.status(200).json({
+            success: true,
+            data: dataToFetchById,
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            data: "This ID does not exist in our DB",
+        });
+    }
 };
 
-const deleteData = (req, res) => {
-    console.log(req.params);
-    res.status(200).json({
-        success: true,
-        data: "delete data from db",
-    });
+//to update data, create a variable for the followiing in the try catch block
+// 1. get the id req.params.id
+// 2. get body of the data to update
+// 3. create an options object and set the value of new to true
+//Use the .findByIdAndUpdate() method to update a document in your DB
+const updateData = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const body = req.body;
+        const options = {
+            new: true, //this tells me to return the body of the update data
+        };
+
+        const dataToUpDate = await Model.findByIdAndUpdate(id, body, options);
+        res.status(200).json({
+            success: true,
+            data: dataToUpDate,
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            data: "This ID does not exist in our DB",
+        });
+    }
+};
+
+//to implementt delet1
+//first, find the id
+//then call the method .findByIdAndDelete()
+const deleteData = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const dataToDelete = await Model.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            data: `${dataToDelete.name} has been deleted successfully`,
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            data: "Id of this element does not exist in our database, so therefore we cannot complete this action",
+        });
+    }
 };
 
 module.exports = {
